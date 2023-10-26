072067e8d5b073eab55365e4cbd6d71643632ae5
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-10-27 03:46:52 +0500

    start app configurations

--- server/src/index.js
+++ server/src/index.js
@@ -6,3 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb+srv://alexander:q123Q123xyz123@recipes.molcnuh.mongodb.net/recipes?retryWrites=true&w=majority")
+
+app.listen(3001, () => console.log("SERVER STARTED!"))
